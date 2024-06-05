2f42f077456806eea9c821b5ee5d4a1b062030fb
Mohamed Elleuch <mohamed.elleuch@amplicade.com>
2024-06-05 11:02:19 +0100

    feature: add authMiddleware to verify token

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const userController = require("../controllers/UserController");
+const authMiddleware = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
-router.post("/users", userController.createUser);
-router.get("/users", userController.getAllUsers);
-router.get("/users/:id", userController.getUserById);
-router.put("/users/:id", userController.updateUserById);
-router.delete("/users/:id", userController.deleteUserById);
+router.post("/users", authMiddleware, userController.createUser);
+router.get("/users", authMiddleware, userController.getAllUsers);
+router.get("/users/:id", authMiddleware, userController.getUserById);
+router.put("/users/:id", authMiddleware, userController.updateUserById);
+router.delete("/users/:id", authMiddleware, userController.deleteUserById);
 
 module.exports = router;
